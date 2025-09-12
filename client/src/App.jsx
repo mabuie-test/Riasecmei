60254e36987594e6a80144c28a4e1193b19b7b1b
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-12 15:07:35 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,24 @@
+// client/src/App.jsx
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
 import Login from './components/Login'
 import Register from './components/Register'
 import Dashboard from './components/Dashboard'
-import AdminCompare from './components/AdminCompare'
+import AdminCompareMulti from './components/AdminCompareMulti'
+import ShowStorage from './components/ShowStorage' // debug (remover quando não precisar)
 
 export default function App(){
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path='/' element={<Dashboard/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/register' element={<Register/>} />
-        <Route path='/admin/compare' element={<AdminCompare/>} />
+        <Route path='/admin/compare' element={<AdminCompareMulti/>} />
+        {/* rota de debug - remover em produção */}
+        <Route path='/debug' element={<ShowStorage/>} />
       </Routes>
     </BrowserRouter>
   )
